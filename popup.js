f285d4ddebadf044bd5baa671116d036551e123f
Abhishek <singhsterabhi@gmail.com>
2016-11-13 12:24:14 +0530

    styling

--- popup.js
+++ popup.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-    $.get('http://210.212.85.155/', function(response){
+    $.get('http://210.212.85.155/', function(response) {
 
-        var notices, title, author, date;
+        var notices,
+            title,
+            author,
+            date;
         notices = $('li.modal-index', response);
-        $('body').append('<ul id="notices"></ul>')
-        $.each(notices, function(index, value){
+        // $('body').append('<ul id="notices"></ul>')
+        $.each(notices, function(index, value) {
             title = $('div.col-7', value).html();
             author = $('div.auth-index', value).html();
             date = $('div.date-index', value).html();
-            $('#notices').append('<li>' + title + '</li>');
+            $('#notices').append('<li><h4>' + title + '</h4></li><div id="date">'+date+'</div>');
         });
 
     });
